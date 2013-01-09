f3aabe80ced866a90b89474c4feacb26305aeb1c
Grzegorz Godlewski <gg@gitgis.com>
2013-01-09 23:11:04 +0100

    Added Twig chart function

--- JChartsBundle.php
+++ JChartsBundle.php
@@ -3,7 +3,12 @@
 namespace J\ChartsBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\Definition;
 
 class JChartsBundle extends Bundle
 {
+    public function boot()
+    {
+    	$this->container->get('twig')->addExtension(new \J\ChartsBundle\Twig\ChartsExtension());
+    }
 }
---
+++ Twig/ChartsExtension.php
@@ -0,0 +1,22 @@
+<?php
+namespace J\ChartsBundle\Twig;
+
+class ChartsExtension extends \Twig_Extension {
+
+	public function getFunctions()
+	{
+		return array(
+				'chart' => new \Twig_Function_Method($this, 'chart')
+		);
+	}
+
+	public function chart($dataArrays)
+	{
+		return "Here will be Chart";
+	}
+
+	public function getName()
+	{
+		return 'charts_extension';
+	}
+}
