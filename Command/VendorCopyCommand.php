8ed9e9bb2db08719ed8c2f404d0c6897aa527aeb
Grzegorz Godlewski <gg@gitgis.com>
2013-01-10 14:58:55 +0100

    Added support for copying charts libraries into ChartsBundle subdir to allow assets:install

---
+++ Command/VendorCopyCommand.php
@@ -0,0 +1,45 @@
+<?php
+namespace J\ChartsBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Output\Output;
+use Symfony\Component\Finder\Finder;
+
+class VendorCopyCommand extends ContainerAwareCommand {
+	
+	protected function configure()
+	{
+		$this
+		->setName('charts:vendor:copy')
+		->setDefinition(array(
+				new InputArgument('vendorName', InputArgument::REQUIRED, 'Chart vendor name'),
+				new InputArgument('targetDir', InputArgument::REQUIRED, 'Destination directory')
+		))
+		->setDescription('Copy chart vendor files into ChartBundle subdirectory')
+		->setHelp(<<<EOT
+The <info>%command.name%</info> copies chart vendor files into ChartBundle subdirectory
+	
+<info>php %command.full_name% flot/flot dir</info>
+EOT
+		)
+		;
+	}
+
+	protected function execute(InputInterface $input, OutputInterface $output)
+	{
+		$vendorName = rtrim($input->getArgument('vendorName'), '/');
+		$originDir = realpath($this->getContainer()->get('kernel')->getRootDir().'/../vendor/'.$vendorName);
+		$targetDir = rtrim($input->getArgument('targetDir'), '/');
+		
+		echo "Copy chart vendor: ".$originDir.' -> '.$targetDir.PHP_EOL;
+		
+		$filesystem = $this->getContainer()->get('filesystem');
+		$filesystem->remove($targetDir);
+		$filesystem->mkdir($targetDir, 0777);
+		$filesystem->mirror($originDir, $targetDir, Finder::create()->in($originDir));
+	}
+}
---
+++ Composer/LibrariesHandler.php
@@ -0,0 +1,19 @@
+<?php
+namespace J\ChartsBundle\Composer;
+use Sensio\Bundle\DistributionBundle\Composer\ScriptHandler;
+
+class LibrariesHandler extends ScriptHandler {
+	
+	public static function install($event)
+	{
+		$options = self::getOptions($event);
+		$appDir = $options['symfony-app-dir'];
+		
+		$vendorToDir = array ("flot/flot" => "Resources/public/flot");
+		
+		foreach ($vendorToDir as $vendorName => $subDir) {
+			$destDir = realpath(__DIR__.'/../')."/".$subDir;
+			static::executeCommand($event, $appDir, 'charts:vendor:copy '.$vendorName.' '.escapeshellarg($destDir));
+		}		
+	}
+}
