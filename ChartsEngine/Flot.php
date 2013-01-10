75951b51ba29293e0abdd8a9c2bf6a8503a296e5
Grzegorz Godlewski <gg@gitgis.com>
2013-01-10 15:23:53 +0100

    Flot chart display real data

--- ChartsEngine/Flot.php
+++ ChartsEngine/Flot.php
@@ -3,11 +3,36 @@ namespace J\ChartsBundle\ChartsEngine;
 
 class Flot implements ChartsEngineInterace {
 	
-	function setData(array $data) {
+	protected $data;
+	
+	public function setData(array $data) {
 		$this->data = $data;
 	}
 	
-	function getHtml() {
-		return "Here will be Flot chart";
+	public function getHtml() {
+		$placeholder = '<div id="placeholder" style="width:600px;height:300px;"></div>';
+		
+		$script = '<script type="text/javascript">$(function () {';
+		
+		$dataSeriesTxt = '';
+		foreach ($this->data as $dataSeriesKey => $data) {
+			$dataSeriesTxt .= 'd'.$dataSeriesKey.',';
+			$script .= 'var d'.$dataSeriesKey.' = '.$this->translateSerie($data).';';
+		}
+
+		$dataSeriesTxt = rtrim($dataSeriesTxt, ",");
+		
+		$script .= '$.plot($("#placeholder"), [ '.$dataSeriesTxt.' ]);});</script>';
+		
+		return $placeholder.$script;
 	}
+	
+	protected function translateSerie($data) {
+		$retVal = array();
+		foreach ($data as $k=>$v) {
+			$retVal[] = array($k, $v);
+		}
+		return json_encode($retVal);
+	}
+
 }
